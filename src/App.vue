e58dbda152e8d5bb509eaa54423eb97c8915d8f6
jumeaux34 <ravilru02@gmail.com>
2025-06-06 13:57:41 +0300

    Rewrite app with exchange and offices pages

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="80" height="80" />
+    <nav>
+      <RouterLink to="/">Курс</RouterLink>
+      <RouterLink to="/offices">Офисы</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
@@ -24,18 +18,18 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
+  text-align: center;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto 1rem;
 }
 
 nav {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  font-size: 14px;
+  margin-top: 1rem;
 }
 
 nav a.router-link-exact-active {
@@ -67,19 +61,11 @@ nav a:first-of-type {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
   nav {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
-
     padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
